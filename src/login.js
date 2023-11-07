f63d9feccc8a498e9d565f4aa2b0bc9e8e0a8415
Alexander R <coder.elf@gmail.com>
2023-11-07 23:04:30 +0300

    WiP

--- src/login.js
+++ src/login.js
@@ -1,13 +1,13 @@
 import "./scss/index.scss";
-import { getElement } from "./utils";
+// import { getElement } from "./utils";
 
-getElement(".js-call-modal").addEventListener("click", () => {
-  getElement('[class*= "js-modal"]').classList.add("is-visibility");
-});
-
-getElement(".js-cross").addEventListener("click", () => {
-  getElement('[class*= "js-modal"]').classList.remove("is-visibility");
-});
+// getElement(".js-call-modal").addEventListener("click", () => {
+//   getElement('[class*= "js-modal"]').classList.add("is-visibility");
+// });
+//
+// getElement(".js-cross").addEventListener("click", () => {
+//   getElement('[class*= "js-modal"]').classList.remove("is-visibility");
+// });
 
 // getElement('.js-login-submit').addEventListener('click', (e) => {
 //   e.preventDefault();
