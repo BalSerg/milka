b1da866eaa2dd1a1ec1529427d0056c02db625f9
Sergey Balabanov <sb@brainrus.ru>
2023-11-30 17:22:39 +0300

    fonts for others countries in pages index, login, reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./scss/index.scss";
-
+import { getElement, getArrayElements } from "./utils";
 // import $ from "jquery";
 
 // import { getElement, getArrayElements } from "./utils";
@@ -16,3 +16,144 @@ import "./scss/index.scss";
 //   e.preventDefault();
 //   window.location.href = 'rooms.html'
 // })
+
+let language;
+
+language = 'az';
+
+switch (language) {
+  case 'ru':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-ru');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-ru');
+    });
+    break;
+
+  case 'kz':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-kz');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-kz');
+    });
+    break;
+
+  case 'az':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-appetite-helvetica');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-lifehack-helvetica');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-lifehack-helvetica');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-lifehack-helvetica');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-appetite-helvetica');
+    });
+    break;
+
+  case 'uz':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-uz');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-uz');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-uz');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-uz');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('uz');
+    });
+    break;
+
+  case 'kg':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-kz');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-kz');
+    });
+    break;
+
+  case 'mn':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-kz');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-lifehack-pacifico');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-kz');
+    });
+    break;
+
+  case 'ge':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-helvetica');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-helvetica');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-helvetica');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-helvetica');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-helvetica');
+    });
+    break;
+
+  case 'am':
+    getArrayElements('.js-form-name').forEach((item) => {
+      item.classList.add('font-amGuard');
+    });
+    getArrayElements('.js-form-text').forEach((item) => {
+      item.classList.add('font-amGuard');
+    });
+    getArrayElements('.js-form-input').forEach((item) => {
+      item.classList.add('font-amGuard');
+    });
+    getArrayElements('.js-form-error').forEach((item) => {
+      item.classList.add('font-amGuard');
+    });
+    getArrayElements('.js-form-button').forEach((item) => {
+      item.classList.add('font-amGuard');
+    });
+    break;
+}
