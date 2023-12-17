2925140b3d48342184a2d6b4c3daa0a086507d14
Alexander R <coder.elf@gmail.com>
2023-12-17 22:39:53 +0300

    Small fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,24 @@ const CopyPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// Если вставить сюда код метрики, она автоматом
-// окажется во всех HTML-файлах
-const metrikaHTML = ``;
+const metrikaHTML = `
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(95730968, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/95730968" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+`;
 
 const ENV_URLS = {
   register: process.env.NODE_ENV === "production" ? "/register" : "index.html",
@@ -83,6 +98,7 @@ listOfHTML.forEach((html) => {
         ENV_URLS,
       },
       minify: {
+        caseSensitive: true,
         collapseWhitespace: process.env.NODE_ENV === "production",
         removeComments: process.env.NODE_ENV === "production",
         minifyCSS: process.env.NODE_ENV === "production",
